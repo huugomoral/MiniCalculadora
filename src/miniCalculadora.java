b5e312be1882b18bcdb0b0b9ba2d1b857871276b
huugomoral <huguillomr@gmail.com>
2026-04-06 11:28:07 +0100

    absoluto

--- src/miniCalculadora.java
+++ src/miniCalculadora.java
@@ -2,6 +2,8 @@ public class miniCalculadora {
     public void raizCuadrada(double valor) {
         Math.sqrt(valor);
     }
-
+    public void valorAbsoluto(double valor) {
+        Math.abs(valor);
+    }
 
 }
