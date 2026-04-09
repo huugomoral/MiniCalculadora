7c5dd064739b12798be63dae6eadb54341680db9
huugomoral <huguillomr@gmail.com>
2026-04-09 12:33:37 +0200

    Documentacion raiz Cuadrada

--- src/miniCalculadora.java
+++ src/miniCalculadora.java
@@ -1,7 +1,18 @@
+/**
+ * Clase principal.
+ * @author Hugo Moral Rama
+ * @version 1.0
+ */
 public class miniCalculadora {
-    public void raizCuadrada(double valor) {
-        Math.sqrt(valor);
+    /**
+     * <p>Este metodo lo que hace es calcular la raiz cuadrdad del valor proporcionado por el usuario mediante una operacion Math.</p>
+     * @param valor
+     * @return <p>La raiz cuadrada</p>
+     */
+    public double raizCuadrada(double valor) {
+        return Math.sqrt(valor);
     }
-
-
+    public double valorAbsoluto(double valor) {
+        return Math.abs(valor);
+    }
 }
